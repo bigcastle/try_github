1bd040a8f4bf68708de997b3eea2c46d0249ebcc
iTAP <iTAP@iTAP-PC21>
2015-05-08 09:44:55 +0900

    ADDクラスのコミットtry

--- src/try_github/Add.java
+++ src/try_github/Add.java
@@ -1,7 +1,7 @@
 package try_github;
 
 public class Add {
-	public static int Add(int a, int b){
+	public int Add(int a, int b){
 		System.out.println("足し算します");
 		int addResult;
 		addResult = a + b;
